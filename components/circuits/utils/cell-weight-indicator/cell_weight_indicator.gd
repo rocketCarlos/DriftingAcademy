9a8850ad3093cafcc66475c7d62a37e33d93b70b
Carlos Muñoz Sánchez <mscarlos.contact@gmail.com>
2026-03-21 22:58:02 +0100

    Add weight color gradient

--- components/circuits/utils/cell-weight-indicator/cell_weight_indicator.gd
+++ components/circuits/utils/cell-weight-indicator/cell_weight_indicator.gd
@@ -5,9 +5,10 @@ const ALPHA: float = 0.663
 
 func set_weight(value: float, max_weight: float) -> void:
 	$Label.text = str(value).pad_decimals(1)
+	var break_point = max_weight / 2.0
 	color = Color(
-		1.0 if value <= (max_weight / 2.0) else inverse_lerp(0.0, max_weight, value),
-		1.0 if value > (max_weight / 2.0) else 1.0-inverse_lerp(0.0, max_weight, value),
+		1.0 if value > break_point else inverse_lerp(0.0, break_point, value),
+		1.0 if value <= break_point else 1.0-inverse_lerp(break_point, max_weight, value),
 		0.0,
 		ALPHA
 	)
