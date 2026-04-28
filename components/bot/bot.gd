bd6c1673fb60673d415d5f41898fe78219d360b8
Carlos Muñoz Sánchez <mscarlos.contact@gmail.com>
2026-04-28 20:14:02 +0200

    Add bots into circuit

--- components/bot/bot.gd
+++ components/bot/bot.gd
@@ -1,1 +1,16 @@
 extends CharacterBody2D
+
+var wheels: Array[Node] = [null, null, null, null]
+
+func _ready() -> void:
+	wheels = find_children("Wheel?")
+
+
+func set_skin(new_skin: Node) -> void:
+	# set wheel relative positions
+	for i in range(wheels.size()):
+		get_node("Wheel"+str(i+1)).position = new_skin.get_node("Wheel"+str(i+1)).position
+	# set collision shape
+	get_node("CollisionShape2D").shape = new_skin.find_child("CollisionShape2D").shape
+	# set skin
+	get_node("Skin").texture = new_skin.texture
