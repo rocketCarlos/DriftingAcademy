98fa25689d395f1913011fed201b8e0a3b2ecd4a
Carlos Muñoz Sánchez <mscarlos.contact@gmail.com>
2026-06-09 21:35:27 +0200

    Fix bot initial behaviour, adjust slipstream sprite, reduce bot count

--- components/bot/camera_2d.gd
+++ components/bot/camera_2d.gd
@@ -8,11 +8,18 @@ var engine_velocities = {
 
 var current_game_speed: int = 0
 
+var prev_camera: Camera2D
+
+func _ready() -> void:
+	prev_camera = get_viewport().get_camera_2d()
+
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("debug_camera"):
 		enabled = not enabled
 		if enabled:
 			make_current()
+		elif prev_camera:
+			prev_camera.make_current()
 
 	if Input.is_action_just_pressed("toggle_game_speed"):
 		current_game_speed = (current_game_speed + 1) % 3
