9b36256705213870d779450d3924b78c7cde3123
Carlos Muñoz Sánchez <mscarlos.contact@gmail.com>
2026-06-27 17:16:30 +0200

    enhance settings menu display

--- components/ui/SettingsMenu/settings.gd
+++ components/ui/SettingsMenu/settings.gd
@@ -1,11 +1,13 @@
 extends Control
 
-@onready var master_slider: HSlider = $MarginContainer/VBoxContainer/Master/CenterContainer/MasterSlider
-@onready var sfx_slider: HSlider = $MarginContainer/VBoxContainer/SFX/CenterContainer/SFXSlider
+@onready var master_slider: HSlider = $MarginContainer/VBoxContainer/Master/MasterSlider
+@onready var sfx_slider: HSlider = $MarginContainer/VBoxContainer/Specifics/SFX/CenterContainer/SFXSlider
+@onready var music_slider: HSlider = $MarginContainer/VBoxContainer/Specifics/Music/CenterContainer/MusicSlider
 
 func _ready() -> void:
 	master_slider.value = Settings.master_volume
 	sfx_slider.value = Settings.sfx_volume
+	music_slider.value = Settings.music_volume
 
 
 func _on_master_slider_value_changed(value: float) -> void:
