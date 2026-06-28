875da6b7b1ec1dfb1e7f7d8e2cf56e6c6ee0ec59
Carlos Muñoz Sánchez <mscarlos.contact@gmail.com>
2026-06-28 20:29:38 +0200

    Enhance settings scene

--- components/ui/SettingsMenu/settings.gd
+++ components/ui/SettingsMenu/settings.gd
@@ -4,11 +4,21 @@ extends Control
 @onready var sfx_slider: HSlider = $MarginContainer/VBoxContainer/Specifics/SFX/CenterContainer/SFXSlider
 @onready var music_slider: HSlider = $MarginContainer/VBoxContainer/Specifics/Music/CenterContainer/MusicSlider
 
+@onready var master_volume_indicator: Label = $MarginContainer/VBoxContainer/Master/Label/Label
+@onready var music_volume_indicator: Label = $MarginContainer/VBoxContainer/Specifics/Music/Label/Label
+@onready var sfx_volume_indicator: Label = $MarginContainer/VBoxContainer/Specifics/SFX/Label/Label
+
+
 func _ready() -> void:
 	master_slider.value = Settings.master_volume
 	sfx_slider.value = Settings.sfx_volume
 	music_slider.value = Settings.music_volume
 
+
+func _process(_delta: float) -> void:
+	master_volume_indicator.text = str(int(Settings.master_volume * 100))
+	music_volume_indicator.text = str(int(Settings.music_volume * 100))
+	sfx_volume_indicator.text = str(int(Settings.sfx_volume * 100))
 
 func _on_master_slider_value_changed(value: float) -> void:
 	Settings.master_volume = value
