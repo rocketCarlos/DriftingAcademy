76549ecbb1272d718d75531c9eada50eb1f2fbf4
Carlos Muñoz Sánchez <mscarlos.contact@gmail.com>
2026-07-05 14:25:14 +0200

    Add credits

---
+++ components/credits/credits.gd
@@ -0,0 +1,5 @@
+extends Control
+
+
+func _on_text_button_button_clicked() -> void:
+	queue_free()
--- components/menus/MainMenu/main_menu.gd
+++ components/menus/MainMenu/main_menu.gd
@@ -1,11 +1,12 @@
 extends Control
 
+@export var credits_scene: PackedScene
 
 func _on_play_button_clicked() -> void:
 	Router.redirect_to.emit(Router.ROUTE_NAME.SKIN_CIRCUIT_SELECTOR)
 
 func _on_credits_button_clicked() -> void:
-	pass
+	add_child(credits_scene.instantiate())
 
 
 func _on_settings_button_clicked() -> void:
